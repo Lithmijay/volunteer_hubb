d77d2f0d113eed5aa9fdd61af6a4194b875e655c
Lithmijay <jayarathnalith@gmail.com>
2025-07-01 22:14:41 +0530

    create community love page

---
+++ communitylove - Copy (2)/src/App.js
@@ -0,0 +1,11 @@
+import CommunityLove from "./components/CommunityLove";
+
+function App() {
+  return (
+    <div className="App">
+      <CommunityLove />
+    </div>
+  );
+}
+
+export default App;
